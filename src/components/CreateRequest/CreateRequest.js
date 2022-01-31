a8a763c3e734ced5f877240692c4b8ef9b04f6af
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2022-01-31 14:00:57 +0000

    Move result display modal into CreateRequest
    As per ui-ill-components commit 238bbd90 we should not be prescribing
    the inclusion of the result display modal within the search results, so
    we've moved it here

--- src/components/CreateRequest/CreateRequest.js
+++ src/components/CreateRequest/CreateRequest.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { SearchConnectorContainer } from '@ptfs-europe/ill-components';
+import { useIntl, FormattedMessage } from 'react-intl';
+
+import {
+  Button,
+  Modal,
+  ModalFooter
+} from '@folio/stripes/components';
+
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel
+} from '@folio/stripes-components';
+
+import {
+  SearchConnectorContainer,
+  DisplayResult
+} from '@ptfs-europe/ill-components';
 
 const CreateRequest = (props) => {
   const { submission, connector } = props.data;
-  return <SearchConnectorContainer submission={submission} connector={connector} />;
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selected, setSelected] = useState();
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  const intl = useIntl();
+
+  const onResultClick = (event, selectedRow) => {
+    setSelected(selectedRow);
+    setModalOpen(true);
+  }
+
+  const footer = (
+    <ModalFooter>
+      <Button
+        buttonStyle="primary"
+        disabled={!canSubmit}
+        marginBottom0
+      >
+        <FormattedMessage id="ui-ill-components.button.placeRequest" />
+      </Button>
+      <Button onClick={() => setModalOpen(false)} marginBottom0>
+        <FormattedMessage id="ui-ill-components.button.cancel" />
+      </Button>
+    </ModalFooter>
+  );
+
+  return (
+    <>
+      {selected && modalOpen && (
+        <Modal
+          aria-label={intl.formatMessage({ id: "ui-ill-components.button.placeRequest" })}
+          open={modalOpen}
+          label={intl.formatMessage({ id: "ui-ill-components.button.placeRequest" })}
+          footer={footer}
+          size="large"
+        >
+          <Tabs>
+            <TabList label="Create request">
+              <Tab>Item details</Tab>
+              <Tab>Service details</Tab>
+              <Tab>Format details</Tab>
+            </TabList>
+            <TabPanel>
+              <DisplayResult result={selected} />
+            </TabPanel>
+            <TabPanel><>Panel 1</></TabPanel>
+            <TabPanel><>Panel 2</></TabPanel>
+          </Tabs>
+        </Modal>
+      )}
+      <SearchConnectorContainer
+        submission={submission}
+        connector={connector}
+        onResultClick={onResultClick}
+      />
+    </>
+  );
 };
 
 CreateRequest.propTypes = {
